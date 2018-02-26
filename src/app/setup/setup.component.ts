8c1353b77305deee9e456e266bcf05f5e9136232
epopov <eugeniu.popov@endava.com>
2018-02-26 10:15:19 +0200

    Setup page elements done

--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-setup',
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetupComponent implements OnInit {
 
-  constructor() { }
+  levels = [
+    {
+      id: 1,
+      description: 'Easy'
+    },
+    {
+      id: 2,
+      description: 'Medium'
+    },
+    {
+      id: 3,
+      description: 'Hard'
+    }
+  ];
+
+  categories = [
+    {
+      id: 1,
+      description: 'History',
+      selected: true
+    },
+    {
+      id: 2,
+      description: 'Biology',
+      selected: true
+    },
+    {
+      id: 3,
+      description: 'Geography',
+      selected: true
+    }
+  ];
+
+  selectedLevel = this.levels[0];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onLevelSelectionChange(level) {
+    this.selectedLevel = Object.assign({}, this.selectedLevel, level);
+  }
+
+  startQuiz() {
+
+  }
+
+  generateQuestions() {
+
+  }
 }
