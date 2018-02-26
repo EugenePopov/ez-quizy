57c804db2dfd0caeed61efbba883d879cdca4f69
epopov <eugeniu.popov@endava.com>
2018-02-26 09:18:19 +0200

    basic modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {SetupComponent} from './setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'setup', component: SetupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [WelcomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SetupComponent } from './setup/setup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    SetupComponent
   ],
   imports: [
     BrowserModule,
