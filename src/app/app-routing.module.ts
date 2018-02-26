947cc4d9dabaea78cb8ddeb85206a2b5449237a9
epopov <eugeniu.popov@endava.com>
2018-02-26 16:32:08 +0200

    can count correct answers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {SetupComponent} from './setup/setup.component';
+import {QuizComponent} from './quiz/quiz.component';
+import {QuestionsResolverGuard} from './questions-resolver.guard';
 
 const routes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'setup', component: SetupComponent}
+  {path: 'setup', component: SetupComponent},
+  {
+    path: 'quiz', component: QuizComponent, resolve: {
+      quiz: QuestionsResolverGuard
+    }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
 
 export const routingComponents = [WelcomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {AppRoutingModule, routingComponents} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { SetupComponent } from './setup/setup.component';
+import {AppComponent} from './app.component';
+import {SetupComponent} from './setup/setup.component';
 import {QuestionsReaderService} from './questions-reader.service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {QuizComponent} from './quiz/quiz.component';
+import {DataService} from './data.service';
+import {QuestionsResolverGuard} from './questions-resolver.guard';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    SetupComponent
+    SetupComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [QuestionsReaderService, HttpClient],
+  providers: [QuestionsReaderService, HttpClient, DataService, QuestionsResolverGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
